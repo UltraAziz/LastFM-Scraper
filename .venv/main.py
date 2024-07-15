04c795914a5af0a2585f2a686214e66027576059
Aziz <azizhadidi7@gmail.com>
2024-07-15 20:29:58 +0300

    Added song stats

--- .venv/main.py
+++ .venv/main.py
@@ -21,6 +21,7 @@ soup = BeautifulSoup(lastfm.content, "html.parser")  #Converts data into proper 
 
 #Process to get data from grids (artists and albums)
 def get_grid_data(number_of_entries, index):
+    global counter
     counter = 0
     main_text_divs = [div for div in
                       soup.find_all('div',
@@ -74,22 +75,62 @@ def get_grid_data(number_of_entries, index):
 
 
 #Process to get song data
-
-song_tds = [td for td in
+def get_song_data(mode):
+    song_tds = [td for td in
                       soup.find_all('td',
-                                    class_="chartlist-name")]  #A list of all td elements with this class
+                                    class_=f"chartlist-{mode}")]  #A list of all td elements with this class
 
-initial_a_list = [td.find_all('a') for td in
+    initial_a_list = [td.find_all('a') for td in
                       song_tds]  #All links within the td elements
-song_list = []
+    song_list = []
 
-for a in initial_a_list:
-    link = str(a)
-    link = link.replace("<", ">")
-    split_link = link.split(">")
-    song_list.append(split_link[2])
-    print(split_link[2])
+    counter = 0
 
+    for a in initial_a_list:
+
+        counter += 1
+        if counter > int(get_config_data("number_of_recent_tracks")):
+            link = str(a)
+            link = link.replace("<", ">")
+            split_link = link.split(">")
+            song_list.append(split_link[2])
+
+
+    if mode == "name": #Only executes when retrieving songs not their artists
+        play_spans = [span for span in
+                      soup.find_all('span',
+                                    class_="chartlist-count-bar-value")]  #A list of all span elements with this class
+
+    song_plays = []
+    if mode == "name":
+        for span in play_spans:
+            playcount = str(span)
+            playcount = playcount.replace("<", ">")
+            split_playcount = playcount.split(">")
+            plays = split_playcount[2].replace("\n", "")
+            while plays.startswith(" "):
+                plays = plays.lstrip(" ")
+            song_plays.append(plays)
+
+    songs_and_plays = []
+
+    songs = {}
+    if len(song_plays) == len(song_list):
+        for i in range(len(song_list)):
+            songs_and_plays.append(song_list[i])
+            songs_and_plays.append(song_plays[i])
+
+
+    for i in range(0, len(songs_and_plays), 2):
+        songs[songs_and_plays[i]] = songs_and_plays[i + 1]
+
+    if mode == "name":
+        return songs
+    if mode == "artist":
+        return song_list
+
+songs = get_song_data("name")
+top_song_artists = get_song_data("artist")
 
 artists = get_grid_data(get_config_data("number_of_artists"), 2)  #Index numbers acquired through trial and error
 
@@ -104,7 +145,7 @@ for key in artist_list:
 
 with open('Last.fm.txt', 'w') as f:  #Writes data to txt file
 
-    print(f"Last.fm profile data for user: {get_config_data("username")}: \n", file=f)
+    print(f"Last.fm profile data for user: {get_config_data("username")} \n", file=f)
     print("Artist data:\n{", file=f)
     for key in artists:
         print(key, ' : ', artists[key], file=f)
@@ -114,3 +155,13 @@ with open('Last.fm.txt', 'w') as f:  #Writes data to txt file
     for key in albums:
         print(key, ' : ', albums[key], file=f)
     print("}\n", file=f)
+
+    print("Song data:\n{", file=f)
+
+    counter = 0
+    for key in songs:
+        print(key, f"by {top_song_artists[counter]}",' : ', songs[key], file=f)
+        counter += 1
+    print("}\n", file=f)
+
+print("File written!")
